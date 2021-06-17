c8baf44c8b8ba86cb93a6d095988ff13e25802cc
ramjithramadas <ramjithramadas@gmail.com>
2021-06-17 20:54:53 +0530

    added server side props in pages

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,18 +1,67 @@
 import { useRouter } from "next/router";
-import { getFilteredEvents } from "../../dummy-data";
+import { getFilteredEvents } from "../../helpers/api-util";
 import EventList from "../../components/events/event-list";
 import ResultsTitle from "../../components/events/results-title";
 import CommonButton from "../../components/common/common-button";
 import CommonErrorAlert from "../../components/common/common-error-alert";
 
-function FilteredEventsPage() {
+function FilteredEventsPage(props) {
     const router = useRouter();
 
-    const filterData = router.query.slug;
+    // const filterData = router.query.slug;
 
-    if (!filterData) {
-        return <p className="center">Loading...</p>;
+    // if (!filterData) {
+    //     return <p className="center">Loading...</p>;
+    // }
+
+    // const filteredYear = filterData[0];
+    // const filteredMonth = filterData[1];
+
+    // const numYear = +filteredYear;
+    // const numMonth = +filteredMonth;
+
+    if (props.hasError) {
+        return (
+            <>
+                <CommonErrorAlert>
+                    <p>Invalid filter. Please adjust your values!</p>
+                </CommonErrorAlert>
+                <div className="center">
+                    <CommonButton link="/events">Show All Events</CommonButton>
+                </div>
+            </>
+        );
     }
+
+    const filteredEvents = props.events;
+
+    if (!filteredEvents || filteredEvents.length === 0) {
+        return (
+            <>
+                <CommonErrorAlert>
+                    <p>No events found for the chosen filter!</p>
+                </CommonErrorAlert>
+                <div className="center">
+                    <CommonButton link="/events">Show All Events</CommonButton>
+                </div>
+            </>
+        );
+    }
+
+    const date = new Date(props.date.year, props.date.month - 1);
+
+    return (
+        <>
+            <ResultsTitle date={date} />
+            <EventList events={filteredEvents} />
+        </>
+    );
+}
+
+export async function getServerSideProps(context) {
+    const { params } = context;
+
+    const filterData = params.slug;
 
     const filteredYear = filterData[0];
     const filteredMonth = filterData[1];
@@ -29,44 +78,29 @@ function FilteredEventsPage() {
         numMonth < 1 ||
         numMonth > 12
     ) {
-        return (
-            <>
-                <CommonErrorAlert>
-                    <p>Invalid filter. Please adjust your values!</p>
-                </CommonErrorAlert>
-                <div className="center">
-                    <CommonButton link="/events">Show All Events</CommonButton>
-                </div>
-            </>
-        );
+        return {
+            props: { hasError: true },
+            // notFound:true
+            // redirect:{
+            //     destination: "/error"
+            // }
+        };
     }
 
-    const filteredEvents = getFilteredEvents({
+    const filteredEvents = await getFilteredEvents({
         year: numYear,
         month: numMonth,
     });
 
-    if (!filteredEvents || filteredEvents.length === 0) {
-        return (
-            <>
-                <CommonErrorAlert>
-                    <p>No events found for the chosen filter!</p>
-                </CommonErrorAlert>
-                <div className="center">
-                    <CommonButton link="/events">Show All Events</CommonButton>
-                </div>
-            </>
-        );
-    }
-
-    const date = new Date(numYear, numMonth - 1);
-
-    return (
-        <>
-            <ResultsTitle date={date} />
-            <EventList events={filteredEvents} />
-        </>
-    );
+    return {
+        props: {
+            events: filteredEvents,
+            date: {
+                year: numYear,
+                month: numMonth,
+            },
+        },
+    };
 }
 
 export default FilteredEventsPage;
